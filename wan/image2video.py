6323a7f25cc0e72d9e1c560d6d5e729348499fb4
intervitens <intervitens@tutanota.com>
2025-03-03 12:02:09 +0300

    Support i2v frame_num other than 81

--- wan/image2video.py
+++ wan/image2video.py
@@ -213,22 +213,22 @@ class WanI2V:
         seed = seed if seed >= 0 else random.randint(0, sys.maxsize)
         seed_g = torch.Generator(device=self.device)
         seed_g.manual_seed(seed)
-        noise = torch.randn(
-            16,
-            21,
-            lat_h,
-            lat_w,
-            dtype=torch.float32,
-            generator=seed_g,
-            device=self.device)
 
-        msk = torch.ones(1, 81, lat_h, lat_w, device=self.device)
+        latent_frame_num = (F - 1) // self.vae_stride[0] + 1
+        noise = torch.randn(16,
+                            latent_frame_num,
+                            lat_h,
+                            lat_w,
+                            dtype=torch.float32,
+                            generator=seed_g,
+                            device=self.device)
+        msk = torch.ones(1, F, lat_h, lat_w, device=self.device)
         msk[:, 1:] = 0
         msk = torch.concat([
             torch.repeat_interleave(msk[:, 0:1], repeats=4, dim=1), msk[:, 1:]
         ],
                            dim=1)
-        msk = msk.view(1, msk.shape[1] // 4, 4, lat_h, lat_w)
+        msk = msk.view(1, latent_frame_num, 4, lat_h, lat_w)
         msk = msk.transpose(1, 2)[0]
 
         if n_prompt == "":
@@ -257,7 +257,7 @@ class WanI2V:
                 torch.nn.functional.interpolate(
                     img[None].cpu(), size=(h, w), mode='bicubic').transpose(
                         0, 1),
-                torch.zeros(3, 80, h, w)
+                torch.zeros(3, F-1, h, w)
             ],
                          dim=1).to(self.device)
         ])[0]
